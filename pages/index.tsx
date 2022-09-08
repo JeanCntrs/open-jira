416dc0d0e5baf0500c94dae12c6e06891fd670c7
Jean C. Contreras <jean.cntrs@gmail.com>
2022-09-08 15:13:11 -0400

    Added light and dark theme

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
+import { Typography } from '@mui/material';
 import type { NextPage } from 'next';
 
 const HomePage: NextPage = () => {
     return (
-        <h1>Hello!</h1>
+        <Typography variant='h1' color='primary'>Hello!</Typography>
     )
 }
 
